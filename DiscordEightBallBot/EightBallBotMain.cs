05885e7ef76a95ea32a4430142fccfabc601e160
Chris Koehler <acomputerdog@gmail.com>
2021-01-21 22:32:37 -0500

    Protect against bot ever trying to talk to itself

--- DiscordEightBallBot/EightBallBotMain.cs
+++ DiscordEightBallBot/EightBallBotMain.cs
@@ -40,6 +40,12 @@ namespace DiscordEightBallBot
         {
             try
             {
+                // Don't reply to ourself
+                if (e.Author.Equals(sender.CurrentUser))
+                {
+                    return;
+                }
+                
                 // Check permissions
                 if (!e.Channel.IsPrivate)
                 {
